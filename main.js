156cebafe6e6c50f937b612e51bc4a672be6676c
madisec <madiisec@gmail.com>
2023-10-25 17:37:36 +0330

    Config project

---
+++ main.js
@@ -0,0 +1,8 @@
+const server = Bun.serve({
+    port: 8080,
+    fetch(request) {
+      return new Response("Welcome to Bun!");
+    },
+  });
+  
+  console.log(`Listening on localhost:${server.port}`);
